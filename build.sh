60a3379e2745ee3f1e1b70e1cc0a0aee22947a41
Huang Yaming <yumminhuang@gmail.com>
2020-07-21 00:00:03 +0800

    Fix build.sh by adding sudo for apt-get

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 # THIS SCRIPT IS USED TO AUTO-BUILD
 
 # install texlive 2019 and dependencies
-apt-get update && \
-apt-get install -y --no-install-recommends \
-    fonts-font-awesome \
-    texlive-fonts-recommended \
-    texlive-formats-extra \
-    texlive-lang-chinese \
-    texlive-latex-extra
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
+        fonts-font-awesome \
+        texlive-fonts-recommended \
+        texlive-formats-extra \
+        texlive-lang-chinese \
+        texlive-latex-extra
 
 # run xelatex
 xelatex Resume.tex -interaction=nonstopmode
